086b59514891cd7a402412d5c352234c283613a8
dfu3 <dfu1357531>
2017-01-24 01:13:10 -0500

    added cell class

--- main.cpp
+++ main.cpp
@@ -4,30 +4,38 @@
 using namespace std;
 const int SIZE = 25; //100 total cells
 
-void fill(string matrix[SIZE][SIZE])
+class Cell
+{
+    public:
+
+        string sym = "";
+
+};
+
+void fill(Cell matrix[SIZE][SIZE])
 {
     //populate matrix
     for (int r=0; r < SIZE-1; r++) {
         for (int c=0; c < SIZE-1; c++) {
-            matrix[r][c] = " # ";
+            matrix[r][c].sym = " # ";
         }
     }
 }
 
-void draw(string matrix[SIZE][SIZE])
+void draw(Cell matrix[SIZE][SIZE])
 {
     //display matrix
     for (int r=0; r < SIZE-1; r++) {
         cout << '\n';
         for (int c=0; c < SIZE-1; c++) {
-            cout << matrix[r][c];
+            cout << matrix[r][c].sym;
         }
     }
 }
 
 int main()
 {
-    string grid[SIZE][SIZE];
+    Cell grid[SIZE][SIZE];
     fill(grid);
     draw(grid);
 
